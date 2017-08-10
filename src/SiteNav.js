f846b2e0a76f4409e287af505330aa49831558c2
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-08-10 21:59:28 +0200

    styles

--- src/SiteNav.js
+++ src/SiteNav.js
@@ -8,7 +8,11 @@ export default function SiteNav() {
       <a className="SiteTitle" href="https://radio4000.com" target="_blank" title="Radio4000.com" rel="noopener noreferrer">
 	<img src={ logo } alt="mix.radio4000.com"/>
       </a>
-      <a href="https://github.com/internet4000/radio4000-mix" className="App-source">github</a>
+      <a href="https://github.com/internet4000/radio4000-mix"
+	 target="_blank"
+	 title="See this site's source code on Github"
+	 rel="noopener noreferrer"
+	 className="App-source">Github</a>
     </nav>
   )
 }
